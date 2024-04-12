69ae4c5855a6a9bac02ada5d4806fa8f13bf2c6d
youbin.jia <youbin.jia@guasemi.com>
2024-04-12 20:25:22 +0800

    Retrieval Chain

--- LangChain_Demo.py
+++ LangChain_Demo.py
@@ -6,9 +6,19 @@ Created on Tue Apr  9 16:59:58 2024
 @author: jyb
 """
 import sys
+import pytest
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.documents import Document
+from langchain.chains import create_retrieval_chain
+
+
 
 class MyAgent:
     def __init__(self, model_name = "gpt-3.5-turbo", temperature = 0):
@@ -20,10 +30,14 @@ class MyAgent:
         self.system_prompt = "You are world class technical documentation writer."
         
     def Chat(self, user_message, verbose=False):
+        if user_message is None:
+            print("user_message is None !")
+            return False
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "{system_prompt}"),
-            ("user", "{user_message}")
+            ("system", self.system_prompt),
+            ("user", user_message)
         ])
+        
         chain = prompt | self.llm
         
         completion = chain.invoke({"user_message" : user_message,
@@ -31,18 +45,25 @@ class MyAgent:
         
         self.ShowUser(user_message)
         self.ShowCompletion(completion, verbose = verbose)
+        return True
         
         
         
     def ShowUser(self, user_message):
         print("#USER# :<<")
         print(user_message, "\n\n")
+        return True
+    
+    def ShowGPT(self, gpt_message):
+        print("#GPT# :>>")
+        print(gpt_message)
+        return True
 
     def ShowCompletion(self, completion, verbose=False):
-        print("#GPT# :>>")
-        print(completion.content)
+        self.ShowGPT(completion.content)
         if verbose:
             self.ShowVerbose(completion)
+        return True
             
             
     def ShowVerbose(self, completion):
@@ -65,8 +86,67 @@ class MyAgent:
         print("{:<20}:{:4}{:<50}".format("finish_reason", " ", finish_reason))
         if logprobs is not None:
             print("{:<20}:{:4}{:<50}".format("logprobs", " ", logprobs))
+        return True;
             
+    def Retrieval(self, user_message):
+        loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+        docs = loader.load()
+        
+        embeddings = OpenAIEmbeddings()
+        
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(docs)
+        vector = FAISS.from_documents(documents, embeddings)
+        
+        prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+            <context>
+            {context}
+            </context>
             
+            Question: {input}""")
+
+        document_chain = create_stuff_documents_chain(self.llm, prompt)
+        # gpt_message = document_chain.invoke({
+        #        "input": user_message,
+        #        "context": [Document(page_content="langsmith can let you visualize test results")]
+        #     })
+        
+        retriever = vector.as_retriever()
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        response = retrieval_chain.invoke({"input": user_message})
+        
+        self.ShowUser(user_message)
+        self.ShowGPT(response["answer"])
+        return True
+
+
+
+def Run(operation):
+    agent = MyAgent()
+    
+    
+    chat_message = "how can langsmith help with testing?"
+    retrieval_message = "how can langsmith help with testing ?"
+    
+    
+    
+    messages = {
+        "Chat": chat_message,
+        "Retrieval": retrieval_message
+    }
+    operations = {
+        "Chat": agent.Chat,
+        "Retrieval": agent.Retrieval
+    }
+    
+    message = messages.get(operation, None)
+    func = operations.get(operation, None)
+    if func:
+        return func(message)
+    else:
+        return "Unsupported operation"
+
+        
 def main():
     """
     langchain demo
@@ -77,10 +157,10 @@ def main():
         #print(param1)
     
     
-    user_message = "how can langsmith help with testing?"
+    operation = "Retrieval"
     
-    agent = MyAgent()
-    agent.Chat(user_message)
+    Run(operation)
+
     
     
 if __name__ == "__main__":
